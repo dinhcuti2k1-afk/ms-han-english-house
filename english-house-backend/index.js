62a14ddc7cc04c523568293a0671427c09a55a48
Vu The Anh <dinhcuti2k1@gmail.com>
2026-02-24 16:35:50 +0700

    backend ready

---
+++ english-house-backend/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Ms.Hân English House Backend Running 🚀");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
